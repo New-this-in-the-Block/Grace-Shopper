0b252adad9f797b299b9237f3a1931a1662fda56
Diana Aguilar <dianaar123@gmail.com>
2020-04-28 21:17:26 -0400

    added some Seq

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
+const {UUID, UUIDV4, STRING, INTEGER, DECIMAL} = Sequelize
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 

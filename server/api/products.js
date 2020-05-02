ed818677b14aca889dc9b6c29724a5f205a106cd
Michael Olsen <molsen@Michaels-MacBook-Air.local>
2020-05-02 10:49:00 -0400

    fixed product route

--- server/api/products.js
+++ server/api/products.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const {Product} = require('../db/db')
 module.exports = router
 
-router.get('/product', (req, res, next) => {
+router.get('/', (req, res, next) => {
   Product.findAll()
     .then(product => res.send(product))
     .catch(next)

9783a90ab32fd77712ab962a1ac7c0e8f2fe06af
Michael Olsen <molsen@Michaels-MacBook-Air.local>
2020-05-02 16:09:14 -0400

    moved login

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -3,8 +3,8 @@ import {useSelector, useDispatch} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
-export default function Nav(props) {
-  const user = useSelector(state => state.user.email)
+export default function Nav() {
+  const user = useSelector(state => state.user)
   const dispatch = useDispatch()
 
   return (
@@ -25,17 +25,20 @@ export default function Nav(props) {
           <Link>Merlot()</Link>
         </div>
       </div>
-      <div id="navLogin">
-        {user ? (
+      {user.id ? (
+        <div id="navLogin">
           <span>{user.email}</span>
-        ) : (
-          <span>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
-          </span>
-        )}
-        <Link>Cart</Link>
-      </div>
+          <a href="#" onClick={() => dispatch(logout())}>
+            Logout
+          </a>
+        </div>
+      ) : (
+        <div id="navLogin">
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Sign Up</Link>
+        </div>
+      )}
+      <Link>Cart</Link>
     </nav>
   )
 }

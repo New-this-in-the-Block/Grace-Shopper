cf3238da0baf4095964ae6a1a342756368a4e0d7
rcntype@gmail.com <rcntype@gmail.com>
2020-05-05 18:01:45 -0400

    added categories component and a route for each category leading to a productCard

--- client/components/ProductDetails.js
+++ client/components/ProductDetails.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
-const ProductDetails = ({currentProduct}, ownProps) => {
+const ProductDetails = ({currentProduct}) => {
   return (
     <div className="detailBox">
       <img
@@ -12,7 +12,7 @@ const ProductDetails = ({currentProduct}, ownProps) => {
         <h1>{currentProduct && currentProduct.name}</h1>
         <h3>${currentProduct && currentProduct.price}</h3>
         <p>{currentProduct && currentProduct.description}</p>
-        <button>Add To Cart</button>
+        <button type="submit">Add To Cart</button>
       </div>
     </div>
   )
@@ -23,8 +23,7 @@ const mapState = ({products}, ownProps) => {
     product => product.id === ownProps.match.params.id
   )
   return {
-    currentProduct,
-    ownProps
+    currentProduct
   }
 }
 

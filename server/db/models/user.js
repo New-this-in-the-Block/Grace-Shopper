a2b751eb540d1976c9db09f3a9d4abf0289cc5c5
rcntype@gmail.com <rcntype@gmail.com>
2020-05-05 23:30:36 -0400

    remove user.password notNull verification

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -15,7 +15,6 @@ const User = db.define('user', {
   },
   password: {
     type: STRING,
-    allowNull: false,
     // Making `.password` act like a func hides it when serializing to JSON.
     // This is a hack to get around Sequelize's lack of a "private" option.
     get() {

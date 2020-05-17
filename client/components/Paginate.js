2169b80a2344677b25ba0ddad29f1eccc92bd804
rcntype@gmail.com <rcntype@gmail.com>
2020-05-17 16:49:31 -0400

    pagination complete i hope

--- client/components/Paginate.js
+++ client/components/Paginate.js
@@ -1,21 +1,50 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import { connect } from 'react-redux'
 
-export default function Paginate ({prodPerPage, totalProds, paginator}) {
+
+function Paginate ({prodPerPage, totalProds, paginator, currentPage}) {
     const pageNums = []
-
-    for (let i=0; i<= Math.ceil(totalProds/prodPerPage); i++){
+    
+    for (let i=0; i<= Math.floor(totalProds/prodPerPage); i++){
         pageNums.push(i+1)
     }
+    
+    const previous = () => {
+        if(currentPage > 1){
+            return currentPage - 1
+        }
+        return currentPage
+    }
+    const next = () => {
+        if(currentPage < pageNums.length){
+            return currentPage + 1
+        }
+        return currentPage
+    }
 
+  
+    // id={pathname < 1 ? 'paginatePage' : ''}
+    if(!currentPage) return null
     return (
         <nav className='pagenav'>
-            <ul className='pagination'>{pageNums.map(page=> (
+            <ul className='pagination'>
+                <li className="page-item"><Link className="page-link" to={`/products/page/${previous()}`} onClick={()=> paginator(previous())}>Previous</Link></li>
+                {pageNums.map(page=> (
                 <li key={page} className='page-item'>
                     <Link to={`/products/page/${page}`} onClick={()=> paginator(page)} className='page-link'>{page}</Link>
                 </li>
-            ))}
+                ))}
+                <li className="page-item"><Link className="page-link" to={`/products/page/${next()}`} onClick={()=> paginator(next())}>Next</Link></li>
             </ul>
         </nav>
-    )
+    ) 
 }
+
+const mapState = (state, ownProps) => {
+    return {
+        ownProps
+    }
+}
+
+export default connect(mapState)(Paginate)
